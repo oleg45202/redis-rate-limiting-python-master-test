dc7d7addcebaa8335c48aa5d6a3984659c83ef32
oleg <olegsh-gusev@mail.ru>
2021-01-12 17:40:07 +0300

    refactoring

--- configuration/urls.py
+++ configuration/urls.py
@@ -53,5 +53,5 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
+    # url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ]
