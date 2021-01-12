233fcb2c6852d0a508bcf7a4af45051b6ae11dc3
oleg <olegsh-gusev@mail.ru>
2021-01-12 15:26:29 +0300

    test

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configuration.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neeley_company.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
