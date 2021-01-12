4331645b20904db4700a270853e5f0d86526387e
oleg <olegsh-gusev@mail.ru>
2021-01-12 16:10:39 +0300

    test

--- configuration/urls.py
+++ configuration/urls.py
@@ -51,6 +51,6 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.index, name='index')
-    # path("", include("projects.urls")),
+    # path('', views.index, name='index')
+    path("", include("core.urls")),
 ]
