8a282d2ad9309cf544937f32f343c5aa02678a49
oleg <gusevoleg96@gmail.ru>
2021-01-13 13:03:35 +0300

    test

--- index.py
+++ index.py
@@ -6,5 +6,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configuration.settings')
 
 app = get_wsgi_application()
 
-os.system("python manage.py collectstatic")
-os.system("python manage.py runserver")
